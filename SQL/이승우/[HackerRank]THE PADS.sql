e1c22dbdda588fa2c17c85ab4c821d87fc5e91c0
brownnyi <brown970523@naver.com>
2025-04-14 15:21:36 +0900

    Update [HackerRank]THE PADS.sql

--- SQL/이승우/[HackerRank]THE PADS.sql
+++ SQL/이승우/[HackerRank]THE PADS.sql
@@ -5,3 +5,5 @@ SELECT CONCAT('There are a total of ', COUNT(Name), ' ', LOWER(Occupation), 's.'
 FROM OCCUPATIONS
 GROUP BY Occupation
 ORDER BY NAME;
+# ORDER 조건에 COUNT랑 Occupation으로 정렬하는 방법 찾아야할듯
+# WITH문으로 정렬된 이름(직업코드) 테이블과 직업(수) 테이블을 만드는 것이 좋을듯
