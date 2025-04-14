cd7674dbbd67a471d6d7a2db756fb4b4e25e0688
brownnyi <brown970523@naver.com>
2025-04-14 10:19:33 +0900

    Create [HackerRank]Occcupations.sql

---
+++ SQL/이승우/[HackerRank]Occcupations.sql
@@ -0,0 +1,8 @@
+SELECT
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS 'Doctor',
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS 'Professor',
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS 'Singer',
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS 'Actor'
+FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) RN
+      FROM Occupations) TEMP
+GROUP BY RN
